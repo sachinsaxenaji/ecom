178855ba47f6c0e1ebe8a7780e1d7e5d646e51dc
sachinsaxenaji <39828137+sachinsaxenaji@users.noreply.github.com>
2021-03-25 15:39:19 +0530

    plus minus button functionality

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u1$p_%ytouxk4oo5anlisid_)%tavn$np%=mbvyb=@58b*a2z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
