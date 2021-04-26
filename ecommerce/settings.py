288f1720942119281e6d00f97d359d659f8113aa
sachinsaxenaji <39828137+sachinsaxenaji@users.noreply.github.com>
2021-04-27 02:53:11 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u1$p_%ytouxk4oo5anlisid_)%tavn$np%=mbvyb=@58b*a2z@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -150,8 +150,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ecommerce',
         'USER': 'postgres',
-        'PASSWORD': '4321',
-        'HOST': 'localhost',
+        'PASSWORD': 'Anand12345',
+        'HOST': 'database-1.cudcszwbj9gu.ap-south-1.rds.amazonaws.com',
         'PORT': '5432'
     }
 }
